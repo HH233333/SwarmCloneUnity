89be1e1e9ef60cac8ad1f0aef48edef4df93dee8
寰星羽 <1328474878@qq.com>
2025-01-21 22:28:17 +0800

    模型通过代码运动的脚本
    通过在资源文件夹中创建资源来生成动画

---
+++ Assets/Script/Live2D_Animation/Chat_Manager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Chat_Manager : MonoBehaviour
+{
+    public TMP_Text chat_text;
+    public TMP_InputField chat_input;
+    public float text_wait_time=0.1f;
+
+    public Model_Manager model_Manager;
+
+    
+
+    public void UpdateInputField_Enter()
+    {
+        SetSomeText(chat_input.text);
+        print("文本输入");
+    }
+    public void SetSomeText(string text)
+    {
+        StartCoroutine(Model_talking(text));
+    }
+    IEnumerator Model_talking(string text)
+    {
+        foreach (var item in text)
+        {
+            chat_text.text += item;
+
+            model_Manager.Is_talking = true;          
+
+            yield return new WaitForSeconds(text_wait_time);
+        }
+        model_Manager.Is_talking = false;
+        yield break;
+    }
+
+
+}
---
+++ Assets/Script/Live2D_Animation/DisplayOnly.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+
+// 设置属性只读
+// 定义一个自定义的属性类DisplayOnly，用于标记某个属性为只读。
+public class DisplayOnly : PropertyAttribute { }
+
+// 使用CustomPropertyDrawer特性来指定这个类是为DisplayOnly属性绘制的自定义抽屉（Drawer）。
+[CustomPropertyDrawer(typeof(DisplayOnly))]
+// 定义一个名为ReadOnlyDrawer的类，它继承自PropertyDrawer，用于自定义属性的绘制方式。
+public class ReadOnlyDrawer : PropertyDrawer
+{
+    // 重写GetPropertyHeight方法，用于获取属性在编辑器中的高度。
+    // 这里直接调用EditorGUI.GetPropertyHeight方法，传入属性、标签和是否包括子属性的参数，返回计算得到的高度。
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUI.GetPropertyHeight(property, label, true);
+    }
+
+    // 重写OnGUI方法，用于在编辑器中绘制属性。
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        // 禁用GUI，使得接下来的绘制操作（即属性字段）变为不可交互（只读）。
+        GUI.enabled = false;
+
+        // 使用EditorGUI.PropertyField方法绘制属性字段，传入位置、属性、标签和是否包括子属性的参数。
+        EditorGUI.PropertyField(position, property, label, true);
+
+        // 重新启用GUI，恢复其交互性，以免影响后续绘制操作。
+        GUI.enabled = true;
+    }
+}
