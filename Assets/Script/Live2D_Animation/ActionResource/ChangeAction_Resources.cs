a0f435764d04ccc6be2f9205da14c0b58822a027
寰星羽 <1328474878@qq.com>
2025-01-29 17:34:10 +0800

    动作资源脚本，用于在指定文件夹下快速创建动作

--- Assets/Script/Live2D_Animation/ActionResource/ChangeAction_Resources.cs
+++ Assets/Script/Live2D_Animation/ActionResource/ChangeAction_Resources.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 [CreateAssetMenu]
 public class ChangeAction_Resources : Action_Resources
 {
-    [Header("头身角度变化区间")]
-    public Vector3 min_vector3;
-    public Vector3 max_vector3;
+
 
 }
--- Assets/Script/Live2D_Animation/ActionResource/ShakeAction_Resources.cs
+++ Assets/Script/Live2D_Animation/ActionResource/ShakeAction_Resources.cs
@@ -6,9 +6,7 @@ using UnityEngine;
 [CreateAssetMenu]
 public class ShakeAction_Resources : Action_Resources
 {
-
+    public float waitTime2 = 0;
     [Header("动作初始变化方向")]
     public int action_direction = 1;
-
-
 }
